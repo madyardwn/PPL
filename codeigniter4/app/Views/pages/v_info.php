2d22bd507994e21eae6f1d1f7da34a3681117286
madyardwn <madyardwn@gmail.com>
2023-02-10 18:16:13 +0700

    Feat v4.3 : Add Author

--- codeigniter4/app/Views/pages/v_info.php
+++ codeigniter4/app/Views/pages/v_info.php
@@ -2,5 +2,8 @@
 <?php echo $this->section('content'); ?>
 
 <h1>Informasi</h1>
+<h3>Tugas ini dibuat untuk memenuhi tugas mata kuliah PPL</h3>
+<h3>disusun oleh : </h3>
+<p>Achmadya Ridwan Ilyawan - 211511001</p>
 
 <?php echo $this->endSection(); ?>
