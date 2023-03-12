a4f785136302363d5e21a29c18250d7878a76c8c
madyardwn <madyardwn@gmail.com>
2023-03-12 14:47:42 +0700

    group routes

--- codeigniter4/app/Config/Routes.php
+++ codeigniter4/app/Config/Routes.php
@@ -35,44 +35,43 @@ $routes->add('hello/(:any)/(:num)', 'C_Home::showme/$1/$2');
 $routes->get('/login', 'C_Auth::index', ['filter' => 'unauthorized']);
 $routes->post('/login', 'C_Auth::login', ['filter' => 'unauthorized']);
 
-// Mahasiswa
 $routes->group(
     'mahasiswa',
+    ['filter' => 'auth'],
     function ($routes) {
-        // dont use add method, because it will be override by auto routing
-        $routes->get('/', 'C_Mahasiswa::index', ['filter' => 'auth']);
-        $routes->get('info', 'C_Info::index', ['filter' => 'auth']);
-        $routes->get('home', 'C_Home::index', ['filter' => 'auth']);
-        $routes->get('logout', 'C_Auth::logout', ['filter' => 'auth']);
+        $routes->get('/', 'C_Mahasiswa::index');
+        $routes->get('info', 'C_Info::index');
+        $routes->get('home', 'C_Home::index');
+        $routes->get('logout', 'C_Auth::logout');
 
-        $routes->post('store/', 'C_Mahasiswa::store', ['filter' => 'auth']);
-        $routes->get('delete/(:num)', 'C_Mahasiswa::delete/$1', ['filter' => 'auth']);
-        $routes->get('show/(:num)', 'C_Mahasiswa::show/$1', ['filter' => 'auth']);
-        $routes->post('update/(:num)', 'C_Mahasiswa::update/$1', ['filter' => 'auth']);
+        $routes->post('store/', 'C_Mahasiswa::store');
+        $routes->get('delete/(:num)', 'C_Mahasiswa::delete/$1');
+        $routes->get('show/(:num)', 'C_Mahasiswa::show/$1');
+        $routes->post('update/(:num)', 'C_Mahasiswa::update/$1');
 
-        $routes->get('edit/(:num)', 'C_Mahasiswa::edit/$1', ['filter' => 'auth']);
-        $routes->get('create', 'C_Mahasiswa::create', ['filter' => 'auth']);
+        $routes->get('edit/(:num)', 'C_Mahasiswa::edit/$1');
+        $routes->get('create', 'C_Mahasiswa::create');
     }
 );
+
 
 // routers pegawai
 $routes->group(
     'pegawai',
+    ['filter' => 'auth'],
     function ($routes) {
-        // dont use add method, because it will be override by auto routing
+        $routes->get('/', 'C_Pegawai::index');
+        $routes->get('info', 'C_Info::index');
+        $routes->get('home', 'C_Home::index');
+        $routes->get('logout', 'C_Auth::logout');
 
-        $routes->get('/', 'C_Pegawai::index', ['filter' => 'auth']);
-        $routes->get('info', 'C_Info::index', ['filter' => 'auth']);
-        $routes->get('home', 'C_Home::index', ['filter' => 'auth']);
-        $routes->get('logout', 'C_Auth::logout', ['filter' => 'auth']);
+        $routes->post('store/', 'C_Pegawai::store');
+        $routes->get('delete/(:num)', 'C_Pegawai::delete/$1');
+        $routes->get('show/(:num)', 'C_Pegawai::show/$1');
+        $routes->post('update/(:num)', 'C_Pegawai::update/$1');
 
-        $routes->post('store/', 'C_Pegawai::store', ['filter' => 'auth']);
-        $routes->get('delete/(:num)', 'C_Pegawai::delete/$1', ['filter' => 'auth']);
-        $routes->get('show/(:num)', 'C_Pegawai::show/$1', ['filter' => 'auth']);
-        $routes->post('update/(:num)', 'C_Pegawai::update/$1', ['filter' => 'auth']);
-
-        $routes->get('edit/(:num)', 'C_Pegawai::edit/$1', ['filter' => 'auth']);
-        $routes->get('create', 'C_Pegawai::create', ['filter' => 'auth']);
+        $routes->get('edit/(:num)', 'C_Pegawai::edit/$1');
+        $routes->get('create', 'C_Pegawai::create');
     }
 );
 
